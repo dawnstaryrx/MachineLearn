c6c4da7ba52cc7c51ddd67c0c9fe149af5afaa61
杨荣兴 <1650396516@qq.com>
2022-06-22 17:32:12 +0800

    agnes

--- 期末试卷/00.py
+++ 期末试卷/00.py
@@ -3,10 +3,4 @@
 # @Author : 杨荣兴
 # @File : 00.py
 # @Software : PyCharm
-import numpy as np
-a = np.repeat(np.arange(5).reshape([1,-1]),10,axis = 0)+10.0
-b = np.random.randint(5, size= a.shape)
-c = np.argmin(a*b, axis=1)
-b = np.zeros(a.shape)
-b[np.arange(b.shape[0]), c] = 1
-print (b)
+import docx
